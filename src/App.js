a11824900a536bb591e9dd05023bda5a014accb8
Amdounislim <amdouni.slim@gmail.com>
2020-11-05 17:45:30 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+import Users from "./components/Users";
+import Comments from "./components/Comments";
+
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" render={() => <Users />} />
+        <Route
+          exact
+          path="/user/:id"
+          render={(props) => <Profile {...props} />}
+        />
+        <Route
+          exact
+          path="/posts/:id"
+          render={(props) => <Comments {...props} />}
+        />
+      </Switch>
     </div>
   );
 }
